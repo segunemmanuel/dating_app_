8ea3de0b19f622a91bc559aefa89e9e4558c6f94
segun <segunemmanuel46@gmail.com>
2021-08-22 15:39:30 -0700

    Update main_nav.blade.php

--- resources/views/layouts/partials/main_nav.blade.php
+++ resources/views/layouts/partials/main_nav.blade.php
@@ -37,7 +37,7 @@
                                 <!-- Header-btn -->
                                 <div class="header-btn d-none f-right d-lg-block">
                                     <a href="{{ route('register')}}" class="btn head-btn1">Register</a>
-                                    <a href="#" class="btn head-btn2">Login</a>
+                                    <a href="{{route('login')}}" class="btn head-btn2">Login</a>
                                 </div>
                             </div>
                         </div>
