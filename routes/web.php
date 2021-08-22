43738fa5d2ec116b69e19dc0d953ff581c653591
segun <segunemmanuel46@gmail.com>
2021-08-22 00:00:41 -0700

    New pages

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,27 @@ use Illuminate\Support\Facades\Route;
 */    
 
 Route::get('/', function () {
+    return view('index');
+});
+
+
+
+Route::get('/dash', function () {
     return view('dash');
 });
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/profiles', function () {
+    return view('profile');
+});
+
+
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
